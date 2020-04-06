143eeb7f5c52e1e57a170773e043a4e246833c3b
Sevi <correosevi@gmail.com>
2020-04-06 19:22:18 +0100

    Public folder and src added

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var express = require('express');
+var socket = require('socket.io');
 
 //Setting up de App
 var app = express();
 var server = app.listen(4000, () => {
 	console.log('Escuchando en el puerto 4000');
+});
+
+// Public folder for Static files
+
+app.use(express.static('public'));
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', (socket) => {
+	console.log('made socket connection');
 });
