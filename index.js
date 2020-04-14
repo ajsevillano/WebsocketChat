346e697eeb34a4bf3194bc152f77aa9c1a6a5ae5
Sevi <correosevi@gmail.com>
2020-04-14 17:12:05 +0100

    Some code cleaning

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-var express = require('express');
-var socket = require('socket.io');
-var ent = require('ent'); // Blocks HTML characters (security equivalent to htmlentities in PHP)
-const port = 4000;
+let express, socket, ent, server, app, port;
 
 //Setting up de App
-var app = express();
-var server = app.listen(port, () => {
+port = 4000;
+express = require('express');
+socket = require('socket.io');
+ent = require('ent'); // Blocks HTML characters (security equivalent to htmlentities in PHP)
+app = express();
+server = app.listen(port, () => {
 	console.log(`Listen up at port ${port}`);
 });
 
-Colors = [
-	'#c0392b',
-	'#d35400',
-	'#457b9d',
-	'#8e44ad',
-	'#27ae60',
-	'#16a085',
-	'#2980b9',
-	'#2c3e50',
-	'#474787',
-];
-
 function getTimeWithLeadingZeros() {
-	var today = new Date();
-	var minutesTwoDigitsWithLeadingZero = ('0' + today.getMinutes()).substr(-2);
-	var secondsTwoDigitsWithLeadingZero = ('0' + today.getSeconds()).substr(-2);
-	var time =
+	let today,
+		minutesTwoDigitsWithLeadingZero,
+		secondsTwoDigitsWithLeadingZero,
+		time;
+
+	today = new Date();
+	minutesTwoDigitsWithLeadingZero = ('0' + today.getMinutes()).substr(-2);
+	secondsTwoDigitsWithLeadingZero = ('0' + today.getSeconds()).substr(-2);
+	time =
 		today.getHours() +
 		':' +
 		minutesTwoDigitsWithLeadingZero +
@@ -35,7 +29,6 @@ function getTimeWithLeadingZeros() {
 }
 
 // Public folder for Static files
-
 app.use(express.static('public'));
 
 //Socket setup
@@ -47,7 +40,7 @@ io.on('connection', (socket) => {
 	console.log('Client ' + socket.id + ' connected');
 
 	socket.on('Newconnection', (name) => {
-		var time = getTimeWithLeadingZeros();
+		let time = getTimeWithLeadingZeros();
 		userData['name'] = name;
 		userData['time'] = time;
 		socket.broadcast.emit('userConnected', userData);
