b101c8dcdd2aade134ffea74d6dab77ee90dcb26
Sevi <correosevi@gmail.com>
2022-02-11 13:50:28 +0000

    Change port

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let express, socket, ent, server, app, port;
 
 //Setting up de App
-port = 4000;
+port = 3000;
 express = require('express');
 socket = require('socket.io');
 ent = require('ent'); // Blocks HTML characters (security equivalent to htmlentities in PHP)
 app = express();
 server = app.listen(port, () => {
-	console.log(`Listen up at port ${port}`);
+  console.log(`Listen up at port ${port}`);
 });
 
 // Public folder for Static files
@@ -19,29 +19,29 @@ var io = socket(server);
 const userData = {};
 
 io.on('connection', (socket) => {
-	let numberOfClients = io.engine.clientsCount;
-	io.sockets.emit('ClientsCounter', numberOfClients);
+  let numberOfClients = io.engine.clientsCount;
+  io.sockets.emit('ClientsCounter', numberOfClients);
 
-	socket.on('Newconnection', (name) => {
-		userData[socket.id] = name;
-		socket.broadcast.emit('userConnected', name);
-	});
+  socket.on('Newconnection', (name) => {
+    userData[socket.id] = name;
+    socket.broadcast.emit('userConnected', name);
+  });
 
-	socket.on('disconnect', () => {
-		if (userData[socket.id] != null) {
-			let updateClients = io.engine.clientsCount;
-			UserUpdate = [userData[socket.id], updateClients];
-			socket.broadcast.emit('userDisconnected', UserUpdate);
-			delete userData[socket.id];
-		}
-	});
+  socket.on('disconnect', () => {
+    if (userData[socket.id] != null) {
+      let updateClients = io.engine.clientsCount;
+      UserUpdate = [userData[socket.id], updateClients];
+      socket.broadcast.emit('userDisconnected', UserUpdate);
+      delete userData[socket.id];
+    }
+  });
 
-	socket.on('chat', (data) => {
-		data.message = ent.encode(data.message);
-		io.sockets.emit('chat', data);
-	});
+  socket.on('chat', (data) => {
+    data.message = ent.encode(data.message);
+    io.sockets.emit('chat', data);
+  });
 
-	socket.on('typing', (data) => {
-		socket.broadcast.emit('typing', data);
-	});
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data);
+  });
 });
